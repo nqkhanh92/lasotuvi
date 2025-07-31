54cf5b0437b5ddec9a99db78c49b15b5ced45048
nqkhanh92 <nqkhanh92@yahoo.com>
2025-07-31 15:03:49 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='lasotuvi',
-      version='0.1.1',
+      version='0.1.2',
       description='Chương trình an sao tử vi mã nguồn mở',
       url='https://github.com/doanguyen/lasotuvi',
       author='doanguyen',
@@ -10,4 +10,15 @@ setup(name='lasotuvi',
       packages=['lasotuvi'],
       setup_requires=['pytest-runner'],
       tests_require=['pytest'],
+      install_requires=[
+        "attrs>=23.1.0",
+        "ephem>=4.1",
+        "more-itertools>=10.0",
+        "mypy>=1.7",
+        "pluggy>=1.3",
+        "py>=1.11.0",
+        "pytest>=8.0",
+        "six>=1.16.0",
+        "typed-ast>=1.5.5"
+      ],
       zip_safe=False)
